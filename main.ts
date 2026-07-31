4deb36014ca4a26542506be0006cbc61d47b9b7a
tchater-dev <t.chater@bthstu.catholic.edu.au>
2026-07-31 10:25:46 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+namespace SpriteKind {
+    export const Friendly = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setPosition(0, 0)
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    1 2 1 2 1 2 1 2 1 2 1 2 1 2 1 2 
+    2 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 2 
+    2 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 2 
+    2 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 2 
+    2 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 2 
+    2 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 2 
+    2 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 2 
+    2 . . . . . . . . . . . . . . 1 
+    1 . . . . . . . . . . . . . . 2 
+    2 1 2 1 2 1 2 1 2 1 2 1 2 1 2 1 
+    `, SpriteKind.Player)
+let Landing_craft = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . c . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Friendly)
+grid.moveWithButtons(mySprite)
+tiles.setCurrentTilemap(tilemap`level1`)
